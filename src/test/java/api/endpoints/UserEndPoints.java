5b21ac2d00d4567cc898aa438a22f7accb46a69c
vinayaksolankure <vinayaksolankure9284@gmail.com>
2024-05-21 09:44:41 +0530

    committing for RestAPIAutomationFramework project

---
+++ src/test/java/api/endpoints/UserEndPoints.java
@@ -0,0 +1,61 @@
+package api.endpoints;
+
+import api.payload.user;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+
+public class UserEndPoints {
+
+	public static Response createUser(user payload) {
+
+		Response response = given()
+				.accept(ContentType.JSON)
+				.contentType(ContentType.JSON)
+				.body(payload)
+
+				.when()
+				.post(Routes.post_url);
+
+		return response;
+	}
+
+	public static Response getUser(String userName) {
+
+		Response response = given()
+				.accept(ContentType.JSON)
+				.pathParam("username", userName)
+
+				.when()
+				.get(Routes.get_url);
+
+		return response;
+	}
+	
+	public static Response updateUser(String userName, user payload) {
+
+		Response response = given()
+				.accept(ContentType.JSON)
+				.contentType(ContentType.JSON)
+				.pathParam("username", userName)
+				.body(payload)
+
+				.when()
+				.put(Routes.put_url);
+
+		return response;
+	}
+	
+	public static Response deleteUser(String userName) {
+
+		Response response = given()
+				.accept(ContentType.JSON)
+				.pathParam("username", userName)
+
+				.when()
+				.delete(Routes.del_url);
+
+		return response;
+	}
+}
